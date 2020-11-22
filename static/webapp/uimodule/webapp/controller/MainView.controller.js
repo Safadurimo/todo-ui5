ba666b111cdc5a51738a025016d14793cfd6e3ea
Martin Böschen <martinboeschen@hotmail.com>
2020-11-22 17:40:17 +0100

    creating and deleting tasks in the ui works

--- static/webapp/uimodule/webapp/controller/MainView.controller.js
+++ static/webapp/uimodule/webapp/controller/MainView.controller.js
@@ -3,37 +3,67 @@ sap.ui.define([
 ], function (Controller) {
   "use strict";
 
+  var updatePage = function (obj) {
+    var oModel = new sap.ui.model.json.JSONModel();
+    obj.getView().setModel(oModel);
+    var data = oModel.loadData("/tasks");
+
+    oModel.attachRequestCompleted(function (data) {
+      var d = { "items": oModel.getData() };
+      oModel.setData(d);
+    });
+
+  };
+
   return Controller.extend("com.safadurimo.todofrontend.todofrontend.controller.MainView", {
 
-    onInit: function(){
-      var oModel = new sap.ui.model.json.JSONModel();
-      this.getView().setModel(oModel);
-      var data = oModel.loadData("/tasks");
-
-      oModel.attachRequestCompleted(function (data) {
-        var d= {"items" : oModel.getData()};
-        console.log("AddressModel: " + d);
-        oModel.setData(d);
-      });
+    onInit: function () {
+      updatePage(this);
     },
 
     onPressSave: function () {
-     
 
+      var task = this.getView().byId("task").getValue();
+      var that = this;
       console.log("saved");
       $.ajax({
         url: '/tasks',
         type: 'POST',
-        data: JSON.stringify({ "text": "Martin","done":false }),
+        data: JSON.stringify({ "text": task, "done": false }),
         contentType: 'application/json',
         success: function (data) {
           console.log("success" + data);
+          updatePage(that);
         },
         error: function (e) {
           console.log("error: " + e);
         }
       });
+
+
     },
+
+    onPressDelete: function (oEvent) {
+
+      var that = this;
+
+      var model = this.getModel();
+      var path = oEvent.getSource().getBindingContext().getPath();
+      var obj = model.getProperty(path);
+      console.log(obj);
+      $.ajax({
+        url: `/tasks/${obj.id}`,
+        type: 'DELETE',
+        success: function (data) {
+          console.log("object deleted");
+          updatePage(that);
+        },
+        error: function (e) {
+          console.log("error: " + e);
+        }
+      });
+
+    }
 
 
 
